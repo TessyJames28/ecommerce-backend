5e45524e2af1bca13e1561bff2aa1a8cb2c883ae
Tessyjames <tessyjames28@gmail.com>
2025-04-11 17:40:55 +0100

    Added endpoint for google login

--- Horal_Backend/users/urls.py
+++ Horal_Backend/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import RegisterUserView, UserLoginView, UserLogoutView
+from .views import RegisterUserView, UserLoginView, UserLogoutView, GoogleLoginView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
     path("register/", RegisterUserView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("google-login/", GoogleLoginView.as_view(), name="google_login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
 ]
