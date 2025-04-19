0bf30169ee4a4bc89b2eab6e2eec8befdfaa1968
Tessyjames <tessyjames28@gmail.com>
2025-04-19 17:32:14 +0100

    Password reset endpoints

--- Horal_Backend/users/urls.py
+++ Horal_Backend/users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import RegisterUserView, UserLoginView, UserLogoutView, GoogleLoginView
+from .views import(
+    RegisterUserView,
+    UserLoginView,
+    UserLogoutView,
+    GoogleLoginView,
+    PasswordResetRequestView,
+    VerifyOTPView,
+    PasswordResetConfirmView,
+)
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
-
+from django.conf import settings
 
 urlpatterns = [
     path("register/", RegisterUserView.as_view(), name="register"),
@@ -10,5 +18,16 @@ urlpatterns = [
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('password-reset/request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
+    path('password-reset/verify-otp/', VerifyOTPView.as_view(), name='password-reset-verify-otp'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 
 ]
+
+
+# Only add in DEBUG mode
+if settings.DEBUG:
+    from .views import OTPTestingView
+    urlpatterns += [
+        path('dev/get-otp/', OTPTestingView.as_view(), name='get-otp-for-testing'),
+    ]
