86e2e6cc363297834efb59c1331c5f34471ac195
Tessyjames <tessyjames28@gmail.com>
2025-05-02 18:14:00 +0100

    Endpoints for seller verification

--- Horal_Backend/sellers/urls.py
+++ Horal_Backend/sellers/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import SellerKYCIDVerificationView, SellerSocialsView, SellerKYCProofOfAddressView
+from .views import (
+    SellerKYCIDVerificationView, SellerSocialsView,
+    SellerKYCProofOfAddressView, VerifiedSeller,
+)
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -8,4 +11,6 @@ urlpatterns = [
     path("kyc/id-verification/", SellerKYCIDVerificationView.as_view(), name="seller_kyc"),
     path("kyc/proof-of-address/", SellerKYCProofOfAddressView.as_view(), name="seller_kyc_proof_of_address"),
     path("socials/", SellerSocialsView.as_view(), name="seller_socials"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('seller-verified/', VerifiedSeller.as_view(), name='verified_seller'),
+    
+]
