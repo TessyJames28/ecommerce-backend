c737512e764c4539bb95a2d524cf8fb91b05d74f
Tessyjames <tessyjames28@gmail.com>
2025-05-02 18:15:58 +0100

    Modified model to inlude superuser

--- Horal_Backend/users/models.py
+++ Horal_Backend/users/models.py
@@ -61,6 +61,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     password = models.CharField(max_length=128, null=True, blank=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_seller = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
