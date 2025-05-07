21d606118e3dc310e275d8b2e5c86d4c643c36a3
Tessyjames <tessyjames28@gmail.com>
2025-05-07 12:17:02 +0100

    Products url

---
+++ Horal_Backend/products/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # Category endpoints
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category-create'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/<uuid:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('categories/<uuid:pk>/view/', views.SingleCategoryDetailView.as_view(), name='single_category-detail'),
+
+    # Product endpoints
+    path('', views.ProductListView.as_view(), name='product-list'),
+    path('<str:category_name>/create/', views.ProductCreateView.as_view(), name='product-create'),
+    # Product endpoint to retrieve, update, and delete products
+    path('<str:category_name>/<uuid:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    
+    # Shop products endpoints
+    path('shop/<uuid:shop_id>/get-products/', views.ShopProductListView.as_view(), name='shop-products'),
+    
+    # Product variant endpoint
+    path('variants/<uuid:variant_id>/', views.ProductVariantView.as_view(), name='product-variant'),
+
+]
