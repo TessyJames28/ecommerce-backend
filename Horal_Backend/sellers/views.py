d8d361622198ce5c1afc9ecf9918913518c1271b
Tessyjames <tessyjames28@gmail.com>
2025-05-07 12:17:24 +0100

    Added shop management

--- Horal_Backend/sellers/views.py
+++ Horal_Backend/sellers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import SellerKYC, SellerSocials
@@ -5,8 +6,9 @@ from .serializers import SellerKYCFirstScreenSerializer, SellerKYCProofOfAddress
 from rest_framework.response import Response
 from rest_framework import status
 from users.models import CustomUser
-from products.models import Shop
-from products.serializers import ShopSerializer
+from products.utility import IsSuperAdminPermission, BaseResponseMixin
+from .models import Shop
+from .serializers import ShopSerializer
 
 # Create your views here.
 class SellerKYCIDVerificationView(GenericAPIView):
@@ -164,3 +166,68 @@ class VerifiedSeller(GenericAPIView):
             },
             status=status.HTTP_201_CREATED
         )
+    
+
+class CreateShop(GenericAPIView):
+    """
+    API endpoints for the superadmins to create shop
+    To use and create platform base products for sale.
+    """
+    serializer_class = ShopSerializer
+    queryset = Shop.objects.all()
+    permission_classes = [IsSuperAdminPermission]
+
+    def post(self, request, *args, **kwargs):
+        """
+        A post method to create a shop by the superuser
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        response_data = {
+            "status": "success",
+            "status_code": status.HTTP_201_CREATED,
+            "message": "Successfully created shop",
+            "data": {
+                "id": str(user.id),
+                "owner_type": user.owner_type,
+                "owner": user.owner,
+                "name": user.name,
+                "created_by_admin": user.created_by_admin,
+                "location": user.location
+            }
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
+
+class ShopManagementView(GenericAPIView, BaseResponseMixin):
+    """
+    API endpoints for superadmins to manage shops
+    """
+    serializer_class = ShopSerializer
+    queryset = Shop.objects.all()
+    permission_classes = [IsSuperAdminPermission]
+
+
+    def get(self, request, *args, **kwargs):
+        """Get all shops"""
+        shops = self.get_queryset()
+        serializer = self.get_serializer(shops, many=True)
+        return self.get_response(
+            status.HTTP_200_OK,
+            "ALL shops retrieved successfully",
+            serializer.data
+        )
+    
+
+    def delete(self, request, pk, *args,**kwargs):
+        """Delete a shop and all its products"""
+        shop = get_object_or_404(Shop, pk=pk)
+        shop.delete() # This will cascade delete all products belonging to the shop
+        return Response({
+            "status": "success",
+            "status_code": status.HTTP_204_NO_CONTENT,
+            "message": "Shop deleted successfully with all its products"
+        })
