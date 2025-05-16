a4fb2dbdc85c047494bfa38f8b7c1334cfaf0b8f
Tessyjames <tessyjames28@gmail.com>
2025-05-16 21:21:03 +0100

    Registered product models

--- Horal_Backend/products/admin.py
+++ Horal_Backend/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (
+    ChildrenProduct, GadgetProduct, AccessoryProduct,
+    VehicleProduct, ElectronicsProduct, FashionProduct,
+    HealthAndBeautyProduct, ProductVariant, FoodProduct,
+)
 
 # Register your models here.
+# admin.site.register(BaseProduct)
+admin.site.register(ChildrenProduct)
+admin.site.register(GadgetProduct)
+admin.site.register(VehicleProduct)
+admin.site.register(ElectronicsProduct)
+admin.site.register(FashionProduct)
+admin.site.register(HealthAndBeautyProduct)
+admin.site.register(ProductVariant)
+admin.site.register(AccessoryProduct)
+admin.site.register(FoodProduct)
+# admin.site.register(ProductCondition)
+# admin.site.register(PowerSource)
