23858f8030d38ec4678a895179aa0ac388320a73
Tessyjames <tessyjames28@gmail.com>
2025-05-16 21:25:08 +0100

    Added location endpoints

--- Horal_Backend/users/urls.py
+++ Horal_Backend/users/urls.py
@@ -7,6 +7,9 @@ from .views import(
     PasswordResetRequestView,
     VerifyOTPView,
     PasswordResetConfirmView,
+    CreateLocationView,
+    SingleLocationView,
+    LocationUpdateDeleteView
 )
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from django.conf import settings
@@ -21,6 +24,9 @@ urlpatterns = [
     path('password-reset/request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('password-reset/verify-otp/', VerifyOTPView.as_view(), name='password-reset-verify-otp'),
     path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('location/add/', CreateLocationView.as_view(), name="user_location"),
+    path('location/<uuid:pk>/view/', SingleLocationView.as_view(), name="single_user_location"),
+    path('location/<uuid:pk>/', LocationUpdateDeleteView.as_view(), name="location_update_delete"),
 
 ]
 
