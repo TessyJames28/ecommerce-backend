ed65a50914f3e000af7fbd30ed6cd5330f295394
Tessyjames <tessyjames28@gmail.com>
2025-05-16 21:20:26 +0100

    Implemented swagger api documentation

--- Horal_Backend/Horal_Backend/urls.py
+++ Horal_Backend/Horal_Backend/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Swagger API configuration
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Horal Backend API",
+        default_version="v1",
+        description="API documentation for Horal backend with django framework",
+        terms_of_service="http://127.0.0.1:8000/terms/",
+        contact=openapi.Contact(email="tessyjames28@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +41,11 @@ urlpatterns = [
     path('api/v1/seller/', include('sellers.urls')),
     path('api/v1/product/', include('products.urls')),
     path('api/v1/cart/', include('carts.urls')),
+    path('api/v1/orders', include('orders.urls')),
     # path('api/v1/profiles/', include('profiles.urls')),
+
+    # Swagger & Redoc URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
