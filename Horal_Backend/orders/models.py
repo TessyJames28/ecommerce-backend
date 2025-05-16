0401ecf3724a92eba99f1a9a866402b4b7208302
Tessyjames <tessyjames28@gmail.com>
2025-05-16 21:19:13 +0100

    Order model

---
+++ Horal_Backend/orders/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from users.models import CustomUser
+from products.models import ProductVariant
+import uuid
+
+
+# Create your models here.
+class Order(models.Model):
+    class Status(models.TextChoices):
+        """Enum for order status"""
+        PENDING = "pending", "Pending"
+        PAID = "paid", "Paid"
+        FAILED = "failed", "Failed"
+        CANCELLED = "cancelled", "Cancelled"
+        SHIPPED = "shipped", "Shipped"
+        DELIVERED = "delivered", "Delivered"
+
+
+    """Model to represents a user's placed order"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(max_length=50, choices=Status.choices, default=Status.PENDING)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.user}"
+    
+
+class OrderItem(models.Model):
+    """Model that represent each variant ordered in the order"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    @property
+    def total_price(self):
+        return self.unit_price * self.quantity
+    
+
+    def __str__(self):
+        return f"{self.variant} X {self.quantity}"
