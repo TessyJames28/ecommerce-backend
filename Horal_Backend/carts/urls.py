7c1f2f26b04247c4ce45bfc10a0cc2f985c14387
Tessyjames <tessyjames28@gmail.com>
2025-05-20 20:47:00 +0100

    Added a url for cart delete method

--- Horal_Backend/carts/urls.py
+++ Horal_Backend/carts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     CartView, CartItemUpdateDeleteView, 
     CartItemCreateView, MergeCartView,
+    CartDeleteView
 )
 
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('add/', CartItemCreateView.as_view(), name='add-to-cart'),
     path('item/<uuid:item_id>/', CartItemUpdateDeleteView.as_view(), name='cart-item-update-delete'),
     path('merge/', MergeCartView.as_view(), name='merge-cart'),
+    path('<uuid:cart_id>/', CartDeleteView.as_view(), name="delete-cart"),
 
 ]
