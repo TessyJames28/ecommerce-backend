256204d01390699176bbbb0873b2d1a03a16bb16
Tessyjames <tessyjames28@gmail.com>
2025-05-20 20:48:21 +0100

    Endpoint for delete method

--- Horal_Backend/orders/urls.py
+++ Horal_Backend/orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CheckoutView, PaymentCallbackView
+from .views import CheckoutView, PaymentCallbackView, OrderDeleteView
 
 
 urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('payment-callback/', PaymentCallbackView.as_view(), name='payment-callback'),
-    
+    path('<uuid:pk>/delete/', OrderDeleteView.as_view(), name='order-deletion'),
 ]
