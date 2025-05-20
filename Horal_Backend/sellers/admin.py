ebdf63a2264de9fb74a8221561fb562dc0534dd0
Tessyjames <tessyjames28@gmail.com>
2025-05-20 20:52:01 +0100

    Django files

--- Horal_Backend/sellers/admin.py
+++ Horal_Backend/sellers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SellerKYC, SellerSocials, Shop
 
 # Register your models here.
+admin.site.register(SellerSocials)
+admin.site.register(SellerKYC)
+admin.site.register(Shop)
