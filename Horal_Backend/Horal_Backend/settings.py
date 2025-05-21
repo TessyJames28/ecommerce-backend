677075ffbcd14b5ca89b4941f55f05bf99105e1b
Tessyjames <tessyjames28@gmail.com>
2025-05-21 12:17:39 +0100

    Still on swagger

--- Horal_Backend/Horal_Backend/settings.py
+++ Horal_Backend/Horal_Backend/settings.py
@@ -226,7 +226,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
-LOGIN_URL = '/admin/login/'  # Redirect to admin login instead of /accounts/login/
 CSRF_TRUSTED_ORIGINS = [
     'https://horal-backend.up.railway.app',
 ]
