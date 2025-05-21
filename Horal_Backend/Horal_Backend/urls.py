59b7eed3446e07f919fe1103447f39ff393b176a
Tessyjames <tessyjames28@gmail.com>
2025-05-21 10:28:33 +0100

    Fixed swagger API view for annonymous user

--- Horal_Backend/Horal_Backend/urls.py
+++ Horal_Backend/Horal_Backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.shortcuts import redirect
 
 # Swagger API configuration
 schema_view = get_schema_view(
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny]
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
@@ -42,10 +42,26 @@ urlpatterns = [
     path('api/v1/product/', include('products.urls')),
     path('api/v1/cart/', include('carts.urls')),
     path('api/v1/order/', include('orders.urls')),
-    # path('api/v1/profiles/', include('profiles.urls')),
-
-    # Swagger & Redoc URLs
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Optional: Redirect from root to Swagger UI
+    path('', lambda request: redirect('schema-swagger-ui')),
 ]
+
+# Swagger + Redoc Docs
+urlpatterns += [
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
+]
+
