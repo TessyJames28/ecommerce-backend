79cf927d9ff7a939154cd58a2b21be86c8d54835
Tessyjames <tessyjames28@gmail.com>
2025-05-21 11:23:31 +0100

    Still working on bug

--- Horal_Backend/Horal_Backend/urls.py
+++ Horal_Backend/Horal_Backend/urls.py
@@ -32,7 +32,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.IsAuthenticated],
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[], # Explicitly empty to prevent authentication
 )
 
 urlpatterns = [
