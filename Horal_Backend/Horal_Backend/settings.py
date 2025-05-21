31242622b2d41eca0c7fb5ec162738a06373f00a
Tessyjames <tessyjames28@gmail.com>
2025-05-21 12:07:48 +0100

    Still on swagger

--- Horal_Backend/Horal_Backend/settings.py
+++ Horal_Backend/Horal_Backend/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
     'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', # Don't Touch! Override in your view
+        'rest_framework.permissions.AllowAny', # Don't Touch! Override in your view
     ],    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 30,
