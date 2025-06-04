e5461d633b44330ee8985e3792a59c84a19d153b
Tessyjames <tessyjames28@gmail.com>
2025-06-04 21:37:54 +0100

    Updated order serializer to include pictures

--- Horal_Backend/orders/serializer.py
+++ Horal_Backend/orders/serializer.py
@@ -1,16 +1,59 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
 from carts.models import Cart, CartItem
+from carts.serializers import CartItemSerializer
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
     """Serializer for Order Item model"""
     total_price = serializers.SerializerMethodField()
+    product = serializers.SerializerMethodField()
+    variant_detail = serializers.SerializerMethodField()
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'variant', 'quantity', 'unit_price', 'total_price']
+        fields = ['id', 'variant', 'quantity', 'unit_price', 'total_price', 'product', 'variant_detail']
 
+
+    def get_product(self, obj):
+        """Get product"""
+        product = obj.variant.product
+
+        # Retrieve product image
+        image_url = None
+        if hasattr(product, 'images'):
+            images = product.images.all()
+            if images.exists():
+                image_url = images.first().url
+
+        return {
+            'id': str(product.id),
+            'title': product.title,
+            'price': str(product.price),
+            'category': product.category.name if hasattr(product, 'category') else None,
+            'subcategory': product.sub_category.name if hasattr(product, 'sub_category') else None,
+            'type': product.__class__.__name__,
+            'image': image_url
+        }
+    
+    def get_variant_detail(self, obj):
+        variant = obj.variant
+
+        if variant.standard_size:
+            custom_size = obj.variant.standard_size
+        elif variant.custom_size_value:
+            custom_size = obj.variant.custom_size_value
+        else:
+            custom_size = None
+
+        return {
+            'id': str(variant.id),
+            'color': variant.color,
+            'custom_size_unit': variant.custom_size_unit,
+            'custom_size': custom_size,
+            'stock_quantity': variant.stock_quantity,
+            'price_override': str(variant.price_override) if variant.price_override else None
+        }
 
     def get_total_price(self, obj):
         return obj.total_price
