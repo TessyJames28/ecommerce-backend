86eac42202933ff11c65ce98c629ed05e372a2a3
Tessyjames <tessyjames28@gmail.com>
2025-06-23 03:31:53 +0100

    Order management endpoint

--- Horal_Backend/orders/urls.py
+++ Horal_Backend/orders/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import CheckoutView, PaymentCallbackView, OrderDeleteView
+from .views import (
+    CheckoutView, PaymentCallbackView, OrderDeleteView,
+    AdminAllOrderView, UserOrderListView, OrderDetailView
+)
 
 
 urlpatterns = [
+    path('<uuid:order_id>/', OrderDetailView.as_view(), name='order-detail'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('payment-callback/', PaymentCallbackView.as_view(), name='payment-callback'),
-    path('<uuid:pk>/delete/', OrderDeleteView.as_view(), name='order-deletion'),
+    path('<uuid:pk>/', OrderDeleteView.as_view(), name='order-deletion'),
+    path('admin/all/', AdminAllOrderView.as_view(), name='admin-all-orders'),
+    path('user-orders/', UserOrderListView.as_view(), name='user-order-list'),
+
 ]
