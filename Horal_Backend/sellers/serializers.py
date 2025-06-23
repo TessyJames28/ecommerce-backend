f09ed2df788f879beff66d03feb8a1c0b5761981
Tessyjames <tessyjames28@gmail.com>
2025-06-23 03:41:29 +0100

    Refactored the serializer

--- Horal_Backend/sellers/serializers.py
+++ Horal_Backend/sellers/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import SellerKYC, SellerSocials, Shop
+from .models import SellerKYC, SellerSocials
 
 
 class SellerKYCFirstScreenSerializer(serializers.ModelSerializer):
@@ -80,30 +80,3 @@ class SellerSocialsSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
     
-
-class ShopSerializer(serializers.ModelSerializer):
-    """Serializer to handle seller's shops"""
-    class Meta:
-        model = Shop
-        fields = '__all__'
-        read_only_fields = ['id']
-
-
-    def create(self, validated_data):
-        """Create a Shop instance"""
-        self.context['request'].user
-        self.validated_data["created_by_admin"] = True
-        return super().create(validated_data)
-    
-
-    def validate(self, attrs):
-        """Validate shop if for third party sellers"""
-        owner_type = attrs.get("owner_type")
-        owner = attrs.get("owner")
-
-        if owner_type == Shop.OwnerType.SELLER and not owner:
-            raise serializers.ValidationError({
-                "seller": "Seller must be set for seller-owned shop."
-            })
-        
-        return attrs
