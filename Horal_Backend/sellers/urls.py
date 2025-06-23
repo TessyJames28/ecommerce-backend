db846a5cebe8e0c9870c5180dd8fd31edad5576c
Tessyjames <tessyjames28@gmail.com>
2025-06-23 03:41:52 +0100

    Endpoints for the refactored views classes

--- Horal_Backend/sellers/urls.py
+++ Horal_Backend/sellers/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from .views import (
     SellerKYCIDVerificationView, SellerSocialsView,
     SellerKYCProofOfAddressView, VerifiedSeller,
-    ShopManagementView, CreateShop
 )
-from django.conf.urls.static import static
-from django.conf import settings
 
 
 urlpatterns = [
@@ -13,11 +10,4 @@ urlpatterns = [
     path("kyc/proof-of-address/", SellerKYCProofOfAddressView.as_view(), name="seller_kyc_proof_of_address"),
     path("socials/", SellerSocialsView.as_view(), name="seller_socials"),
     path('seller-verified/', VerifiedSeller.as_view(), name='verified_seller'),
-
-    # Shop management (superadmin only)
-    path('shop/', ShopManagementView.as_view(), name='shop-list'),
-    path('shop/create/', CreateShop.as_view(), name="create-shop"),
-    # superadmin can delete the shop
-    path('shop/<uuid:pk>/', ShopManagementView.as_view(), name='shop-detail'),
-    
 ]
