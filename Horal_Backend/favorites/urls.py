750e8226cfdd46c979511bcb3db88906da956959
Tessyjames <tessyjames28@gmail.com>
2025-06-23 03:52:52 +0100

    Url endpoint for favorite management

--- Horal_Backend/favorites/urls.py
+++ Horal_Backend/favorites/urls.py
@@ -11,7 +11,7 @@ app_name = 'favorites'
 urlpatterns = [
     path('', FavoritesView.as_view(), name="favorite-list"),
     path('add/', AddToFavoritesView.as_view(), name="add-to-favorites"),
-    path('remove/<uuid:item_id>/', RemoveFromFavoritesView.as_view(), name='remove-from-favorites'),
+    path('<uuid:item_id>/', RemoveFromFavoritesView.as_view(), name='remove-from-favorites'),
     path('check/', CheckFavoriteStatusView.as_view(), name="check-favorite-status"),
     path('merge/', MergeFavoritesView.as_view(), name="merge-favorites"),
 
