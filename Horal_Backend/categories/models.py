3ee76d001db2e97cd4eaa4ec754f2acbbcbfaa91
Tessyjames <tessyjames28@gmail.com>
2025-06-23 03:47:31 +0100

    Model for product categorea

---
+++ Horal_Backend/categories/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Category(models.Model):
+    """Model for product categories."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
