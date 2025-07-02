cea18a1f788183b82c62a5e59d407ffdb5b00878
Tessyjames <tessyjames28@gmail.com>
2025-07-02 21:27:28 +0100

    Features for paystack payment integration

---
+++ Horal_Backend/payment/views.py
@@ -0,0 +1,310 @@
+from django.conf import settings 
+import requests, hmac, hashlib, json
+from django.db import transaction
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import GenericAPIView
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from .models import PaystackTransaction
+from .serializers import PaystackTransactionSerializer
+from orders.models import Order
+from rest_framework.views import APIView
+from carts.models import CartItem
+from .utility import trigger_refund, update_order_status
+from orders.serializer import OrderSerializer
+from products.utility import update_quantity, IsAdminOrSuperuser
+from django.utils.decorators import method_decorator
+import uuid
+
+
+# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
+class InitializeTransaction(APIView):
+    """
+    class to initialize order payment on paystack
+    """
+
+    def post(self, request):
+        """Function to handle payment initialization on paystack"""
+        email = request.data.get("email")
+        order_id = request.data.get('order_id') # passed from frontend
+        print(f"email: {email}\norder_id: {order_id}")
+        
+        try:
+            order = Order.objects.get(id=order_id, user__email=email)
+        except Order.DoesNotExist:
+            return JsonResponse({
+                "status": "error",
+                "status_code": status.HTTP_404_NOT_FOUND,
+                "message": "Order not found"
+            }, status=status.HTTP_404_NOT_FOUND)
+        
+        amount = int(order.total_amount * 100) # convert amount to kobo
+        reference = str(uuid.uuid4())
+
+        # Step 1: Initialize payment with Paystack
+        url = "https://api.paystack.co/transaction/initialize"
+        headers = {
+            "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "email": email,
+            "amount": amount,
+            "reference": reference,
+            "metadata": {
+                "order_id": str(order.id)
+            }
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        res_data = response.json()
+
+        if not res_data.get("status"):
+            return JsonResponse({
+                "status": "error",
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": "Paystack error"
+            })
+
+        # Step 2: Save the transaction
+        PaystackTransaction.objects.create(
+            reference=reference,
+            user=request.user,
+            email=email,
+            amount=amount,
+            order=order,
+            status=PaystackTransaction.StatusChoices.PENDING
+        )
+
+        return Response(res_data)
+    
+
+@method_decorator(csrf_exempt, name='dispatch')
+class VerifyTransaction(APIView):
+    """
+    Class to verify paystack transaction if successful
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, reference):
+        """Method to verify user payment by admin"""
+        url = f"{settings.PAYSTACK_VERIFY_URL}/{reference}"
+        headers = {
+            "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}"
+        }
+
+        response = requests.get(url, headers=headers)
+        res_data = response.json()
+
+        try:
+            tx = PaystackTransaction.objects.get(reference=reference)
+        except PaystackTransaction.DoesNotExist:
+            return JsonResponse({
+                "status": "error",
+                "status_code": status.HTTP_404_NOT_FOUND,
+                "message": "Transaction not found"
+            }, status=status.HTTP_404_NOT_FOUND)
+        
+        if res_data['data']['status'] == "success":
+            # If already marked successful, skip update
+            if tx.status != PaystackTransaction.StatusChoices.SUCCESS:
+                tx.status = PaystackTransaction.StatusChoices.SUCCESS
+                tx.paid_at = res_data['data']['paid_at']
+                tx.gateway_response = res_data['data'].get('gateway_response')
+                tx.save()
+
+                # Check and update order if necessary
+                if tx.order and tx.order.status != Order.Status.PAID:
+                    update_order_status(tx.order, Order.Status.PAID, request.user)
+
+            model_data = Order.objects.get(id=tx.order.id)
+            serializer = OrderSerializer(model_data)
+
+            return JsonResponse({
+                "status": "success",
+                "message": "Payment successful",
+                "order_data": serializer.data,
+                "payment_data": res_data['data']
+            })
+        else:
+            tx.status = PaystackTransaction.StatusChoices.FAILED
+            tx.save()
+            return JsonResponse({
+                "status": "failed",
+                "message": "Payment verification failed",
+                "data": res_data['data']
+            })
+    
+
+@csrf_exempt
+def transaction_webhook(request):
+    """
+    Setup a webhook to listen to transaction events
+    """
+    secret = settings.PAYSTACK_SECRET_KEY.encode()
+    signature = request.headers.get('x-paystack-signature')
+    body = request.body
+    expected_signature = hmac.new(secret, body, hashlib.sha512).hexdigest()
+
+    if signature != expected_signature:
+        return JsonResponse({
+            "status": "error",
+            "status_code": status.HTTP_400_BAD_REQUEST,
+            "message": "Invalid signature"
+        })
+    
+    event = json.loads(body)
+    data = event.get('data', {})
+    event_type = event['event']
+    reference = data.get('reference') or data.get("transaction_reference")
+    if not reference:
+        print(f"[Webhook] Missing reference in payload: {json.dumps(event, indent=2)}")
+        return JsonResponse({
+            "status": "error",
+            "status_code": 400,
+            "message": "Missing transaction reference"
+        }, status=400)
+
+    try:
+        tx = PaystackTransaction.objects.get(reference=reference)
+        order = tx.order
+    except PaystackTransaction.DoesNotExist:
+        return JsonResponse({
+            "status": "error",
+            "status_code": status.HTTP_404_NOT_FOUND,
+            "message": "Transaction not found"
+        })
+    
+    if event_type == 'charge.success':
+        tx.status = PaystackTransaction.StatusChoices.SUCCESS
+        tx.paid_at = data.get('paid_at')
+        tx.gateway_response = data.get('gateway_response')
+        tx.save()
+
+        if order and order.status != Order.Status.PAID:
+            try:
+                with transaction.atomic():
+                    for item in order.order_items.all():
+                        variant = item.variant
+                        variant.reserved_quantity -= item.quantity
+                        variant.save()
+                        update_quantity(variant.product)
+
+                    CartItem.objects.filter(cart__user=order.user).delete()
+                    update_order_status(order, Order.Status.PAID)
+            except Exception:
+                pass
+
+        elif event_type == "charge.failed":
+            tx.status = PaystackTransaction.StatusChoices.FAILED
+            tx.save()
+
+            if order:
+                try:
+                    with transaction.atomic():
+                        for item in order.order_items.all():
+                            variant = item.variant
+                            variant.reserved_squantity -= item.quantity
+                            variant.stock_quantity += item.quantity
+                            variant.save()
+                            update_quantity(variant.product)
+                        update_order_status(order, Order.Status.FAILED)
+                except Exception:
+                    pass
+
+    return JsonResponse({
+        "status": "success",
+        "status_code": status.HTTP_200_OK
+    })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class PaymentCallbackView(GenericAPIView):
+    """Update order status after payment"""
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        """Handles order creation after payment"""
+        order_id = request.data.get('order_id')
+
+        if not order_id:
+            JsonResponse({
+                "status": "error",
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": "order_id is required"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            order = Order.objects.get(id=order_id, user=request.user)
+        except Order.DoesNotExist:
+            return JsonResponse({
+                "status": "error",
+                "status_code": status.HTTP_404_NOT_FOUND,
+                "message": "Order not found"
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        model_data = PaystackTransaction.objects.get(order=order.id)
+        serializer = PaystackTransactionSerializer(model_data)
+        return JsonResponse({
+            "message": f"Order is currently {order.status.lower()}",
+            "status": order.status,
+            "order": serializer.data
+        }, status=status.HTTP_200_OK)
+
+
+class RetryRefundView(APIView):
+    """
+    class to handle refund retry for failed refund cases
+    """
+    permission_classes = [IsAdminOrSuperuser]
+
+    def post(self, request):
+        """
+        Post method for admin and staff to retry refund
+        When refund failed from paystack end
+        """
+        reference = request.data.get("reference")
+        if not reference:
+            return Response({
+                "status": "error",
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": "reference is required"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            tx = PaystackTransaction.objects.get(reference=reference)
+        except PaystackTransaction.DoesNotExist:
+            return Response({
+                "status": "error",
+                "status_code": status.HTTP_404_NOT_FOUND,
+                "message": "Transaction not found"
+            }, status=status.HTTP_404_NOT_FOUND)
+        
+        if tx.refund_successful:
+            return Response({
+                "status": "success",
+                "status_code": status.HTTP_200_OK,
+                "message": "Refund already successful"
+            }, status=status.HTTP_200_OK)
+        
+        result = trigger_refund(reference, retry=True)
+        # Add just before or after calling trigger_refund
+
+        if result.get("status"):
+            return Response({
+                "status": "success",
+                "status_code": status.HTTP_200_OK,
+                "message": "Refund retried and successful"
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "status": "error",
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": result.get("message", "Refund retry failed")  # extract Paystack error if present
+            }, status=status.HTTP_400_BAD_REQUEST)
+
