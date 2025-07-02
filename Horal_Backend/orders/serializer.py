670051fa61d8868ad9b21e79a0d29db897fd233b
Tessyjames <tessyjames28@gmail.com>
2025-07-02 21:21:59 +0100

    Updated order checkout response to include user email

--- Horal_Backend/orders/serializer.py
+++ Horal_Backend/orders/serializer.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
-from carts.models import Cart, CartItem
-from carts.serializers import CartItemSerializer
+from .models import Order, OrderItem, OrderReturnRequest
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -62,12 +60,25 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     """Serializer for order model"""
     items = OrderItemSerializer(many=True, read_only=True, source='order_items')
+    user_email = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'created_at', 'status', 'total_amount', 'items']
+        fields = ['id', 'user', 'user_email', 'created_at', 'status', 'total_amount', 'items']
 
 
     def get_items(self, obj):
         """Retrieve all order items"""
         return OrderItemSerializer(obj.order_items.all(), many=True).data
+    
+    def get_user_email(self, obj):
+        return obj.user.email
+    
+
+class OrderReturnRequestSerializer(serializers.ModelSerializer):
+    """Serializer for order return request"""
+    order = OrderSerializer()
+
+    class Meta:
+        model = OrderReturnRequest
+        fields = "__all__"
