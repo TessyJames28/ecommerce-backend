16e4ec528f7cc0922f58cc2723cbd19974132254
Tessyjames <tessyjames28@gmail.com>
2025-07-02 21:26:31 +0100

    Serializer for paystack transaction model

---
+++ Horal_Backend/payment/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import PaystackTransaction
+from orders.serializer import OrderSerializer
+
+
+class PaystackTransactionSerializer(serializers.ModelSerializer):
+    """Serializer to serialize the PaystackTransaction model"""
+    order = OrderSerializer()
+
+    class Meta:
+        model = PaystackTransaction
+        fields = "__all__"
