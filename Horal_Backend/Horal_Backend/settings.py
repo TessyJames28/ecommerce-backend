e3f18325351835a27bf022f0a1eb72d6e8ce657e
Tessyjames <tessyjames28@gmail.com>
2025-07-05 16:41:32 +0100

    Settings update with postgre url

--- Horal_Backend/Horal_Backend/settings.py
+++ Horal_Backend/Horal_Backend/settings.py
@@ -245,11 +245,17 @@ WSGI_APPLICATION = 'Horal_Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# Postgres DB on render
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL'),
 }
 
 # auth user model
