d1cb031afca189f302fa5dfb89328ea141e52717
Tessyjames <tessyjames28@gmail.com>
2025-07-09 18:46:41 +0100

    Added shipping address as part of profile response data

--- Horal_Backend/user_profile/serializers.py
+++ Horal_Backend/user_profile/serializers.py
@@ -2,19 +2,27 @@ from rest_framework import serializers
 from .models import Profile
 from products.models import ImageLink
 from django.contrib.auth.password_validation import validate_password
+from users.serializers import LocationSerializer, ShippingAddressSerializer
+from users.models import Location
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Serializer for users profile"""
     image = serializers.CharField(required=False)
     current_password = serializers.CharField(write_only=True, required=False)
+    phone_number = serializers.CharField(source='user.phone_number', read_only=True)
     new_password = serializers.CharField(write_only=True, required=False)
     confirm_password = serializers.CharField(write_only=True, required=False)
     email = serializers.EmailField(read_only=True)
+    location = LocationSerializer(source='user.location', read_only=True)
+    shipping_address = ShippingAddressSerializer(source='user.shipping_address', read_only=True)
 
     class Meta:
         model = Profile
-        fields = ['user', 'full_name', 'email', 'image', 'current_password', 'new_password', 'confirm_password']
+        fields = [
+            'user', 'full_name', 'email', 'image', 'current_password', 'new_password',
+            'confirm_password', 'shipping_address', 'location', 'phone_number',
+        ]
         extra_kwargs = {
             'user': {'read_only': True}
         }
@@ -23,6 +31,8 @@ class ProfileSerializer(serializers.ModelSerializer):
         password = validated_data.pop('new_password', None)
         validated_data.pop('confirm_password', None)
         image_url = validated_data.pop("image", None)
+        location = validated_data.pop('user', {}).get('location', None)
+
         if image_url:
             image_obj, _ = ImageLink.objects.get_or_create(
                 url=image_url,
@@ -33,6 +43,12 @@ class ProfileSerializer(serializers.ModelSerializer):
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
+
+        # Update location if provided
+        if location:
+            location = Location.objects.create(user=user, order=instance, **location)
+            location.save()
+
 
         # Update password on the associated CustomUser
         if password:
