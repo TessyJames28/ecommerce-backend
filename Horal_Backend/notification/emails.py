bfa4501ba64de54b3e325c50bbd5b4e81e1eba2f
Tessyjames <tessyjames28@gmail.com>
2025-07-26 13:18:14 +0100

    Email notification for kyc verification

--- Horal_Backend/notification/emails.py
+++ Horal_Backend/notification/emails.py
@@ -19,4 +19,30 @@ def send_refund_email(user_email, order_id):
 #         print(f"[Email MOCK] To: {user_email} | Subject: {subject} | Message: {message}")
 #     else:
 #         send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user_email])
-    
+
+
+def send_kyc_info_completed_email(user):
+    """
+    Send email to users on successful kyc verification submission
+    """
+    send_mail(
+        subject="KYC Verification Submitted",
+        message="You have successfully submitted all required KYC information",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+    )
+
+
+def send_kyc_final_status_email(user, status):
+    """
+    Send email that confirms seller veririfation status
+    """
+    message = "Congratulations! Your KYC was verified." if status == 'verified' else \
+              "Unfortunately, your KYC verification failed. Please try again."
+
+    send_mail(
+        subject="KYC Verification Confirmation",
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+    )
