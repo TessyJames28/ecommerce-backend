33a3c2021bf14967efb298c98e7b58dff5c7eb8f
Tessyjames <tessyjames28@gmail.com>
2025-07-26 13:13:21 +0100

    Updated the textchoices

--- Horal_Backend/products/textchoices.py
+++ Horal_Backend/products/textchoices.py
@@ -28,17 +28,25 @@ class Color(models.TextChoices):
     OLIVE = 'olive', 'Olive'
     BEIGE = 'beige', 'Beige'
     TRANSPARENT = 'transparent', 'Transparent'
+    BRONZE = 'bronze', 'Bronze'
+    NAVY = 'navy', 'Navy'
+    CORAL = 'coral', 'Coral'
+    SALMON = 'salmon', 'Salmon'
+    KHAKI = 'khaki', 'Khaki'
+    LAVENDER = 'lavender', 'Lavender'
+    PEACH = 'peach', 'Peach'
+    MINT = 'mint', 'Mint'
     
 
-class SizeOption(models.Model):
+class SizeOption:
     class SizeUnit(models.TextChoices):
         STANDARD = "standard", "Standard Size (e.g. S, M, L)"
-        INCH = "inch", "Inch"
-        CM = "cm", "Centimeter"
-        KG = "kg", "Kilogram"
-        G = "g", "Gram"
-        ML = "ml", "Mililiter"
-        L = "l", "Liter"
+        INCH = "INCH", "Inch"
+        CM = "CM", "Centimeter"
+        KG = "KG", "Kilogram"
+        G = "G", "Gram"
+        ML = "ML", "Milliliter"
+        L = "L", "Liter"
 
     class StandardSize(models.TextChoices):
         XS = "XS", "Extra Small"
@@ -47,16 +55,25 @@ class SizeOption(models.Model):
         L = "L", "Large"
         XL = "XL", "Extra Large"
         XXL = "XXL", "Double Extra Large"
+        XXXL = "XXXL", "Triple Extra Large"
+        XXXXL = "XXXXL", "Quad Extra Large"
+        XXXXXL = "XXXXXL", "Quint Extra Large"
 
-    value = models.CharField(max_length=20) # e.g., "S", "55", "30"
-    unit = models.CharField(max_length=10, choices=SizeUnit.choices)
-
-    def __str__(self):
-        return f"{self.value} {self.get_unit_display()}" if self.unit != "standard" else self.value
+    
+class AgeRecommendation(models.TextChoices):
+    NEWBORN_0_3M = "0-3m", "0 - 3 Months"
+    INFANT_3_6M = "3-6m", "3 - 6 Months"
+    INFANT_6_12M = "6-12m", "6 - 12 Months"
+    TODDLER_1_2Y = "1-2y", "1 - 2 Years"
+    PRESCHOOL_3_4Y = "3-4y", "3 - 4 Years"
+    EARLY_CHILDHOOD_5_6Y = "5-6y", "5 - 6 Years"
+    CHILD_7_9Y = "7-9y", "7 - 9 Years"
+    PRETEEN_10_12Y = "10-12y", "10 - 12 Years"
+    TEEN_13Y = "13y", "13 Years"
     
 
 class ProductCondition(models.TextChoices):
-    NEW = 'brand_new', 'Brand New'
+    NEW = 'brand new', 'Brand New'
     USED = 'used', 'Used'
 
 
@@ -101,7 +118,7 @@ class Type(models.TextChoices):
 class SkinType(models.TextChoices):
     DRY = 'dry', 'Dry'
     OILY = 'oily', 'Oily'
-    COMBINATION = 'combination', 'Combination'
+    DRY_OILY = 'dry and oily', 'Dry and Oily'
 
 
 class FoodCondition(models.TextChoices):
@@ -121,28 +138,3 @@ class EngineSize(models.TextChoices):
     SMALL = 'small', '1.0-2.0-liter'
     MEDIUM = 'medium', '2.0-3.0-liter'
     LARGE = 'large', '3.0-liter+'
-
-
-class SleeveLength(models.TextChoices):
-    SLEEVELESS = "sleeveless", "Sleeveless"
-    SHORT = "short", "Short Sleeve"
-    THREE_QUARTER = "3/4", "Three Quarter"
-    LONG = "long", "Long Sleeve"
-    CAP = "cap", "Cap Sleeve"
-
-class Neckline(models.TextChoices):
-    ROUND = "round", "Round Neck"
-    V_NECK = "v_neck", "V-Neck"
-    COLLARED = "collared", "Collared"
-    OFF_SHOULDER = "off_shoulder", "Off Shoulder"
-    SWEETHEART = "sweetheart", "Sweetheart"
-    HALTER = "halter", "Halter"
-    TURTLENECK = "turtleneck", "Turtleneck"
-
-
-class Occasion(models.Model):
-    """Occassion options for fashion products"""
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
