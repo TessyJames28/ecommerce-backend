55502ba371b15ff430fd0428d8526af19d845971
Tessy James <tessyjames28@gmail.com>
2025-08-05 14:28:44 +0100

    deleted files

--- Horal_Backend/sellers/scheduler.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from django.conf import settings
-from .tasks import verify_seller_kyc
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(verify_seller_kyc.delay, 'interval', minutes=10)
-    scheduler.start()
