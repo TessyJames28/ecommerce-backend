5e6c888ca4c520f8de9d006e6ddab75a0e570f27
Tessy James <tessyjames28@gmail.com>
2025-08-13 23:06:22 +0100

    Task to auto complete order

--- Horal_Backend/orders/tasks.py
+++ Horal_Backend/orders/tasks.py
@@ -1,5 +1,8 @@
 from celery import shared_task
-from .expire_pending_orders import cancel_expired_pending_orders
+from .order_utils import (
+    cancel_expired_pending_orders,
+    authomatic_order_completion
+)
 
 @shared_task
 def expire_pending_orders_task():
@@ -8,3 +11,12 @@ def expire_pending_orders_task():
     """
     cancel_expired_pending_orders()
     print("✅ Expired pending orders task completed.")
+
+
+@shared_task
+def auto_complete_orders_tasks():
+    """
+    Celery tasks to auto complete orders more than or equal 3 days
+    """
+    authomatic_order_completion()
+    print("✅ Orders delivered over the past 3 days marked as completed.")
