2f89e96afb999c085cf172872e042f9e06b907f5
Tessy James <tessyjames28@gmail.com>
2025-08-13 22:26:04 +0100

    Helper function to validate uuid

--- Horal_Backend/sellers_dashboard/helper.py
+++ Horal_Backend/sellers_dashboard/helper.py
@@ -1,4 +1,6 @@
 from datetime import date, timedelta
+from rest_framework.exceptions import ValidationError
+import uuid
 
 def get_day_start(d: date) -> date:
     return d
@@ -11,3 +13,13 @@ def get_month_start(d: date) -> date:
 
 def get_year_start(d: date) -> date:
     return d.replace(month=1, day=1)
+
+
+def validate_uuid(value):
+    """Function to handle id validation"""
+    try:
+        uuid.UUID(str(value))
+    except ValueError:
+        raise ValidationError("Invalid ID format. Must be a valid UUID.")
+
+    return value
