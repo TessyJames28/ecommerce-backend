f5d0abc969fe5c0e7e23161b78e5af2d3d2d350e
Tessy James <tessyjames28@gmail.com>
2025-08-13 22:46:58 +0100

    Separate dashboard response payload

--- Horal_Backend/sellers_dashboard/urls.py
+++ Horal_Backend/sellers_dashboard/urls.py
@@ -3,7 +3,8 @@ from .views import (
     SellerProductRatingView,
     SellerOrderListView,
     SellerProfileView,
-    SellerDashboardAnalyticsAPIView
+    SellerDashboardAnalyticsAPIView,
+    TopSellingProductsAPIView
 )
 
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('orders/', SellerOrderListView.as_view(), name="seller-orders"),
     path('profile/', SellerProfileView.as_view(), name='seller-profile'),
     path("analytics/", SellerDashboardAnalyticsAPIView.as_view(), name="seller-dashboard-analytics"),
-
+    path("topselling/", TopSellingProductsAPIView.as_view(), name="seller_top_selling_product"),
+    
 ]
