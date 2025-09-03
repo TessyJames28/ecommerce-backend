657899c43ffc71bcac3456e10f442546134cd760
Tessy James <tessyjames28@gmail.com>
2025-09-03 22:21:48 +0100

    Added custom auth

--- Horal_Backend/sellers/views.py
+++ Horal_Backend/sellers/views.py
@@ -11,6 +11,7 @@ from .serializers import (
 from django.utils.timezone import now
 from rest_framework.response import Response
 from rest_framework import status
+from users.authentication import CookieTokenAuthentication
 from products.utils import (
     IsSuperAdminPermission, BaseResponseMixin,
     StandardResultsSetPagination, product_models
@@ -132,6 +133,7 @@ class SellerAddressCreateView(GenericAPIView, BaseResponseMixin):
     """
     serializer_class = SellerKYCAddressSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [CookieTokenAuthentication]
 
     def post(self, request, *args, **kwargs):
         """Method to create or update seller address"""
@@ -176,15 +178,12 @@ class DojahCACWebhook(GenericAPIView, BaseResponseMixin):
     def post(self, request, *args, **kwargs):
         """Post method to handle cac verification webhook"""
         payload = request.data
-        print(f"Payload: {payload}")
+
         # Basic verification
-        cac_payload = payload.get("metadata", {})
-        print(f"Cac payload: {cac_payload}")
         verification_type = payload.get("metadata", {}).get("verification_type")
-        print(f"Verification type: {verification_type}")
         if verification_type != "cac":
             return self.get_response(
-                status.HTTP_400_BAD_REQUEST,
+                status.HTTP_200_OK,
                 "Invalid verification type"
             )
         
@@ -232,12 +231,9 @@ class DojahCACWebhook(GenericAPIView, BaseResponseMixin):
             kyc_cac.save(update_fields=['status'])
 
         return Response({
-            "status": "success",
-            "status_code": status.HTTP_200_OK,
-            "message": "KYC verification completed",
-            "data": SellerKYCCACSerializer(kyc_cac).data,
-            "dojah_response": payload
-        })
+            "status": "ok",
+            "message": "CAC verification successful.",
+        }, status=status.HTTP_200_OK)
 
 
 
@@ -245,6 +241,7 @@ class SellerSocialsView(GenericAPIView):
     """API endpoint for the seller's social media links"""
     serializer_class = SellerSocialsSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [CookieTokenAuthentication]
 
     def post(self, request, *args, **kwargs):
         """Handle the creation of social media links"""
@@ -283,100 +280,3 @@ class SellerSocialsView(GenericAPIView):
             "data": serializer.data
         }, status=status.HTTP_200_OK)
     
-
-class CreateShop(GenericAPIView):
-    """
-    API endpoints for the superadmins to create shop
-    To use and create platform base products for sale.
-    """
-    serializer_class = ShopSerializer
-    queryset = Shop.objects.all()
-    permission_classes = [IsSuperAdminPermission]
-
-    def post(self, request, *args, **kwargs):
-        """
-        A post method to create a shop by the superuser
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-
-        response_data = {
-            "status": "success",
-            "status_code": status.HTTP_201_CREATED,
-            "message": "Successfully created shop",
-            "data": {
-                "id": str(user.id),
-                "owner_type": user.owner_type,
-                "owner": user.owner,
-                "name": user.name,
-                "created_by_admin": user.created_by_admin,
-                "location": user.location
-            }
-        }
-
-        return Response(response_data, status=status.HTTP_201_CREATED)
-
-
-class ShopManagementView(GenericAPIView, BaseResponseMixin):
-    """
-    API endpoints for superadmins to manage shops
-    """
-    serializer_class = ShopSerializer
-    queryset = Shop.objects.all()
-    permission_classes = [IsSuperAdminPermission]
-
-
-    def get(self, request, *args, **kwargs):
-        """Get all shops"""
-        shops = self.get_queryset()
-        serializer = self.get_serializer(shops, many=True)
-        return self.get_response(
-            status.HTTP_200_OK,
-            "ALL shops retrieved successfully",
-            serializer.data
-        )
-    
-
-    def delete(self, request, pk, *args,**kwargs):
-        """Delete a shop and all its products"""
-        shop = get_object_or_404(Shop, pk=pk)
-        shop.delete() # This will cascade delete all products belonging to the shop
-        return Response({
-            "status": "success",
-            "status_code": status.HTTP_204_NO_CONTENT,
-            "message": "Shop deleted successfully with all its products"
-        })
-    
-
-class ShopProductListView(GenericAPIView, BaseResponseMixin):
-    """
-    API endpoint to list all products of a shop
-    """ 
-    permission_classes = [AllowAny]
-    authentication_classes = []
-    pagination_class = StandardResultsSetPagination
-
-    def get(self, request, shop_id, *args, **kwargs):
-        """Get all products of a shop"""
-        shop = get_object_or_404(Shop, pk=shop_id)
-
-        products_data = []
-
-        for model, serializer_class, category_name in product_models:
-            products = model.published.filter(shop=shop)
-            if products.exists():
-                serializer = serializer_class(products, many=True)
-                for product_data in serializer.data:
-                    product_data['category_name'] = category_name
-                    products_data.append(product_data)
-                    print(products_data)
-
-        page = self.paginate_queryset(products_data)
-        if page is not None:
-            paginated_response = self.get_paginated_response(products_data)
-            paginated_response.data["status"] = "success"
-            paginated_response.data["status_code"] = status.HTTP_200_OK
-            paginated_response.data["message"] = "Shop products retrieved successfully"
-        
-        return paginated_response
