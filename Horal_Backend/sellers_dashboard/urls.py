3c9490753722942f50f71c45c31fde90176e6429
Tessy James <tessyjames28@gmail.com>
2025-09-12 19:08:05 +0100

    Endpoint to view order details by sellers

--- Horal_Backend/sellers_dashboard/urls.py
+++ Horal_Backend/sellers_dashboard/urls.py
@@ -4,13 +4,15 @@ from .views import (
     SellerOrderListView,
     SellerProfileView,
     SellerDashboardAnalyticsAPIView,
-    TopSellingProductsAPIView
+    TopSellingProductsAPIView,
+    SellerOrderDetailView
 )
 
 
 urlpatterns = [
     path('reviews/', SellerProductRatingView.as_view(), name="product_reviews"),
     path('orders/', SellerOrderListView.as_view(), name="seller-orders"),
+    path('orders/<uuid:order_item_id>/', SellerOrderDetailView.as_view(), name="seller-order-item-details"),
     path('profile/', SellerProfileView.as_view(), name='seller-profile'),
     path("analytics/", SellerDashboardAnalyticsAPIView.as_view(), name="seller-dashboard-analytics"),
     path("topselling/", TopSellingProductsAPIView.as_view(), name="seller_top_selling_product"),
