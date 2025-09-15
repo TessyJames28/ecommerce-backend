7ce87580088189690b61025a1afb57ef7e2c79e0
Tessy James <tessyjames28@gmail.com>
2025-09-15 17:36:13 +0100

    New signal to handle insufficient fund

--- Horal_Backend/wallet/signals.py
+++ Horal_Backend/wallet/signals.py
@@ -1,12 +1,21 @@
 from .models import SellerTransactionHistory, Payout, SellersBankDetails
 from orders.models import OrderItem, Order
+from .tasks import retry_payout
 from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.dispatch import receiver, Signal
 from django.conf import settings
 from django.utils.timezone import now
 from notifications.tasks import send_email_task
 from django.conf import settings
 import uuid
+
+
+payout_retry_signal = Signal()
+
+@receiver(payout_retry_signal)
+def schedule_payout_retry(sender, payout, **kwargs):
+    # Schedule retries — e.g. every 1 hour up to 5 hours
+    retry_payout.apply_async((payout.id,), countdown=60*300)  # 5 hour later
 
 
 @receiver(post_save, sender=OrderItem)
@@ -112,6 +121,9 @@ def notify_seller_of_initiated_withdrawal(sender, instance, **kwargs):
     if instance.status != Payout.StatusChoices.PROCESSING:
         return
     
+    if instance.email_sent and instance.status == Payout.StatusChoices.PROCESSING:
+        return
+    
     # Get seller bank details
     bank_details = SellersBankDetails.objects.get(seller=instance.seller)
 
@@ -141,3 +153,52 @@ def notify_seller_of_initiated_withdrawal(sender, instance, **kwargs):
 
     # Mark as sent
     Payout.objects.filter(id=instance.id).update(email_sent=True)
+
+
+@receiver(post_save, sender=Payout)
+def notify_seller_of_transfer_success(sender, instance, **kwargs):
+    """
+    Signal to send email to seller after successful transfer
+    """
+    if instance.status not in [
+        Payout.StatusChoices.SUCCESS,
+        Payout.StatusChoices.FAILED
+    ]:
+        return
+    
+    # Get seller bank details
+    bank_details = SellersBankDetails.objects.get(seller=instance.seller)
+
+    recipient = instance.seller.email
+    seller_name = instance.seller.full_name
+    amount = instance.amount_naira
+    transaction_id = instance.id
+    bank_name = bank_details.bank_name
+    from_email = f"Horal Wallet <{settings.DEFAULT_FROM_EMAIL}>"
+
+    if instance.status == Payout.StatusChoices.SUCCESS:
+        body_paragraphs = [
+            f"Congratulations! Your recent withdrawal has been successfully processed. The funds has been delivered to your bank account."
+        ]
+        subject = "Transfer Successful"
+
+    elif instance.status == Payout.StatusChoices.FAILED:
+        body_paragraphs = [
+            f"Unfortunately, we are unable to process your withdrawal for the below transaction. Kindly initiate an another withdrawal.",
+            f"We are sorry for the inconvenience."
+        ]
+        subject = "Transfer Failure"
+
+    send_email_task.delay(
+        recipient=recipient,
+        subject=subject,
+        from_email=from_email,
+        template_name="notifications/emails/withdrawal_template.html",
+        context={
+            "seller_name": seller_name,
+            "body_paragraphs": body_paragraphs,
+            "withdrawal_amount": amount,
+            "transaction_id": transaction_id,
+            "bank_name": bank_name,
+        }
+    )
