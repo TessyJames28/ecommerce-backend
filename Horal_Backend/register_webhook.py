4052438f47afd857f2968afb47c4e99868397208
Tessy James <tessyjames28@gmail.com>
2025-11-06 09:41:06 +0100

    Updated webhook registration file

--- Horal_Backend/register_webhook.py
+++ Horal_Backend/register_webhook.py
@@ -12,10 +12,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Horal_Backend.settings')
 django.setup()
 
 from logistics.utils import register_webhook_view
-from orders.tasks import create_fez_shipment_on_each_shipment
-
-result = create_fez_shipment_on_each_shipment.delay("08069fbd754b4c4cac0f1abd2b89824b")
-print(f"result: {result}")
 
 def register_webhook():
     print("Syncing FEZ Delivery Data to Register Webhook")
@@ -25,5 +21,5 @@ def register_webhook():
     print(f"Registered webhook result: {result}")
 
 
-# if __name__ == "__main__":
-    # register_webhook()
+if __name__ == "__main__":
+    register_webhook()
