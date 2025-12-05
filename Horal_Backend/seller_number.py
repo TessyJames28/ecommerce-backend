45157c72d11e5e8b0037ec621c78161cdb277ec3
Tessy James <tessyjames28@gmail.com>
2025-12-05 12:34:38 +0100

    Updated script

--- Horal_Backend/seller_number.py
+++ Horal_Backend/seller_number.py
@@ -11,3 +11,9 @@ for shop in shops:
         seller = SellerKYC.objects.get(user=shop.owner)
         print(f"Seller Name: {seller.address.first_name} {seller.address.last_name}")
         print(f"\tShop name: {shop.name}\n\tPhone number: {seller.address.mobile}")
+        prods = ProductIndex.objects.filter(shop=shop)
+        for p in prods:
+               if not p.is_published:
+                      print(f"\t\tProduct Name: {p.title}.")
+                      print(f"\t\tCategory: {p.category}.\n")
+
