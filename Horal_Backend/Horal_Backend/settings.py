bcc984191587addaa7c3a043b48bb50fc68c8b3b
Tessy James <tessyjames28@gmail.com>
2025-12-12 16:36:38 +0100

    Updated settings

--- Horal_Backend/Horal_Backend/settings.py
+++ Horal_Backend/Horal_Backend/settings.py
@@ -276,63 +276,63 @@ redis_url = env("REDIS_URL")
 url = urlparse.urlparse(redis_url)
 
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": f"redis://{url.hostname}:{url.port}/0",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # No PASSWORD, no SSL
-        }
-    }
-}
-
-# Celery settings
-CELERY_BROKER_URL = f"redis://{url.hostname}:{url.port}/0"
-CELERY_RESULT_BACKEND = CELERY_BROKER_URL
-
-# No SSL needed for Render internal Redis
-CELERY_BROKER_USE_SSL = None  
-
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
-
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = "UTC"
-
-
 # CACHES = {
 #     "default": {
 #         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": f"{url.scheme}://{url.hostname}:{url.port}/0",
+#         "LOCATION": f"redis://{url.hostname}:{url.port}/0",
 #         "OPTIONS": {
 #             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#             "PASSWORD": url.password,
-#             "SSL": url.scheme == "rediss",
+#             # No PASSWORD, no SSL
 #         }
 #     }
 # }
 
+# # Celery settings
+# CELERY_BROKER_URL = f"redis://{url.hostname}:{url.port}/0"
+# CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 
-# # Celery setting
-# import ssl
+# # No SSL needed for Render internal Redis
+# CELERY_BROKER_USE_SSL = None  
 
-# # Change to its redis DB on production with custom redis setup
-# CELERY_BROKER_URL = env("REDIS_URL")
+# CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
-# CELERY_BROKER_USE_SSL = {
-#     # Change to SSL.CERT_REQUIRED when using own trusted custom redis
-#     'ssl_cert_reqs': None # or ssl.CERT_NONE 
-# }
+# CELERY_ACCEPT_CONTENT = ["json"]
+# CELERY_TASK_SERIALIZER = "json"
+# CELERY_RESULT_SERIALIZER = "json"
+# CELERY_TIMEZONE = "UTC"
 
-# CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
-# CELERY_RESULT_BACKEND = 'django-db'
-# CELERY_ACCEPT_CONTENT = ['json']
-# CELERY_TASK_SERIALIZER = 'json'
-# CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TIMEZONE = 'UTC' 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"{url.scheme}://{url.hostname}:{url.port}/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": url.password,
+            "SSL": url.scheme == "rediss",
+        }
+    }
+}
+
+
+# Celery setting
+import ssl
+
+# Change to its redis DB on production with custom redis setup
+CELERY_BROKER_URL = env("REDIS_URL")
+
+CELERY_BROKER_USE_SSL = {
+    # Change to SSL.CERT_REQUIRED when using own trusted custom redis
+    'ssl_cert_reqs': None # or ssl.CERT_NONE 
+}
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC' 
 
 
 # FEZ Delivery Setup
@@ -361,17 +361,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Horal_Backend.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Postgres DB on render
-DATABASES = {
-    'default': env.db('DATABASE_URL'),
-}
+# DATABASES = {
+#     'default': env.db('DATABASE_URL'),
+# }
 
 # auth user model
 AUTH_USER_MODEL = 'users.CustomUser'
